48b399e2f4355fba8eddd6e1e1230449280c87a5
Basith P <basithpofficial@gmail.com>
2021-06-26 18:30:08 +0530

    Create list item widget

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 
+import '../models/transaction.dart';
+
 class HomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Shoes',
+      amount: 2300,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'Headphones',
+      amount: 1340,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -11,7 +28,35 @@ class HomePage extends StatelessWidget {
       body: Column(
         children: <Widget>[
           Card(
-            child: Text('Trackr'),
+            child: Text('Chart'),
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.teal,
+                          width: 2,
+                        ),
+                      ),
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                      padding: EdgeInsets.all(10),
+                      child: Text(tx.amount.toString()),
+                    ),
+                    Column(
+                      children: [
+                        Text(tx.title),
+                        Text(tx.date.toString()),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
           )
         ],
       ),

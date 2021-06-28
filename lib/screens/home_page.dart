18347e68e6a311c285e1a6a07f9f5e407c146504
Basith P <basithpofficial@gmail.com>
2021-06-28 17:57:00 +0530

    Make add transaction form a seperate widget

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/new_transaction.dart';
 import '../widgets/transaction_list.dart';
 import '../models/transaction.dart';
 
@@ -24,35 +25,7 @@ class HomePage extends StatelessWidget {
             Card(
               child: Text('Chart'),
             ),
-            Card(
-              color: Color(0xff262626),
-              child: Padding(
-                padding: const EdgeInsets.all(15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                      controller: titleController,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Amount'),
-                      controller: amountController,
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton.icon(
-                      onPressed: () => print(titleController.text),
-                      label: Text('Add transaction'),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.teal,
-                        padding: EdgeInsets.all(18),
-                      ),
-                      icon: Icon(Icons.arrow_downward_rounded),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            NewTransaction(),
             TransactionList(),
           ],
         ),
